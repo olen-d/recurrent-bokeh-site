9e0a1e8807da420136f53092507bcf33a7b9d9f4
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-01-02 01:00:32 -0500

    Updated to use rows and columns instead of layout and flex

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,23 +1,23 @@
 <template>
-  <v-layout text-center wrap>
-    <v-flex xs12>
+  <v-row>
+    <v-col align="center" cols="12">
       <v-img
         :src="require('../assets/logo.svg')"
         class="my-3"
         contain
         height="200"
       ></v-img>
-    </v-flex>
+    </v-col>
 
-    <v-flex mb-4>
+    <v-col align="center" cols="12" mb-4>
       <h1 class="display-2 font-weight-bold mb-3">
         {{ welcome.headline }}
       </h1>
       <p class="subheading font-weight-regular">
         {{ welcome.message }}
       </p>
-    </v-flex>
-  </v-layout>
+    </v-col>
+  </v-row>
 </template>
 <script>
 export default {
