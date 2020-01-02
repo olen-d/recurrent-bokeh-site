de944111246057d26d0c1c0a00110b71c0771639
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-01-02 00:19:39 -0500

    Added flexibility section to the front page

---
+++ src/components/FrontPageContent.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row>
+    <v-col align="center" cols="12">
+      <h2 class="display-1 font-weight-bold mb-3">
+        {{ heading }}
+      </h2>
+      <p class="subheading font-weight-regular">
+        {{ content }}
+      </p>
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+  name: "FrontPageContent",
+
+  props: {
+    heading: String,
+    content: String
+  }
+};
+</script>
