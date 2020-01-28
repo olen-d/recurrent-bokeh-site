a6dfb0f0a1d95c96a287656558a24e4f3befb4d9
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-01-27 23:15:53 -0500

    Basic login form functionality

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="admin-login">
+    <Login />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from "@/components/Login.vue";
+
+export default {
+  name: "adminLogin",
+  components: {
+    Login
+  },
+
+  beforeRouteLeave(to, from, next) {
+    this.$store.commit("setLoginPage", false);
+    next();
+  }
+};
+</script>
