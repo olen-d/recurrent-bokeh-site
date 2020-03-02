bbe702d52c2f7a2b3e350af1f60f8e6e87f8291e
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-03-02 00:52:12 -0500

    Backend is now connected to the signup page

--- src/store/store.js
+++ src/store/store.js
@@ -29,6 +29,41 @@ export default new Vuex.Store({
     }
   },
 
+  getters: {
+    jwt: state => state.currentJWT,
+    jwtData: (state, getters) =>
+      state.currentJWT ? JSON.parse(atob(getters.jwt.split(".")[1])) : null,
+    userId: (state, getters) => (getters.jwtData ? getters.jwtData._id : null),
+    userName: (state, getters) =>
+      getters.jwtData ? getters.jwtData.userName : null,
+    firstName: (state, getters) =>
+      getters.jwtData ? getters.jwtData.firstName : null,
+    lastName: (state, getters) =>
+      getters.jwtData ? getters.jwtData.lastName : null,
+    administrator: (state, getters) =>
+      getters.jwtData ? getters.jwtData.administrator : null,
+    editor: (state, getters) =>
+      getters.jwtData ? getters.jwtData.editor : null,
+    expiration: (state, getters) =>
+      getters.jwtData ? getters.jwtData.exp : null,
+    now: state => state.now,
+    authorized: (state, getters) => {
+      const curTime = Math.floor(getters.now.getTime() / 1000);
+      if (getters.jwtData && curTime < getters.jwtData.exp) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    doLogout: state => state.doLogout,
+    loginPage: state => state.loginPage,
+    signupPage: state => state.signupPage,
+    statusCategory: state => state.statusCategory,
+    statusIcon: state => state.statusIcon,
+    statusMessage: state => state.statusMessage,
+    statusAction: state => state.statusAction
+  },
+
   actions: {
     start({ commit }) {
       setInterval(() => {
